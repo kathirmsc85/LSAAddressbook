442aaca259db94977f4913380a47a66e69a98403
Kathiresh Muthusamy <kathireshmuthusamy@gmail.com>
2019-04-24 14:39:28 -0400

    clean up

--- addressbook/views.py
+++ addressbook/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
 from .models import UserProfile, ContactInfo
 from .forms import LoginForm, RegisterForm, ContactInfoForm
 
